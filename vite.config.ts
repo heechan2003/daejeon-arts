e19469ff5422c693e418f251b4ee554222174c31
heechan <101608118+heechan2003@users.noreply.github.com>
2025-04-22 23:00:48 +0900

    ran build on vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
+  base: '/daejeon-arts/',
   build: {
     minify: 'esbuild',
   }
